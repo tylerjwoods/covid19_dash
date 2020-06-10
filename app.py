fa1854b6c481bbf87b6506f452a463c453da3334
Tyler Woods <tyjordan.woods@gmail.com>
2020-06-09 17:57:29 -0700

    finish app inside notebook - move to .py script

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np 
+import plotly.express as px
+import numpy as np
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
